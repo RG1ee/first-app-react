7f28bd27354cc4ab3ebbd340644cd5d09b26efb6
RG1ee <niwows@yandex.ru>
2024-05-08 22:51:28 +0300

    internationalization date

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,11 +1,13 @@
 import "./JournalItem.css";
 
 function JournalItem({ title, date, text }) {
+  const fromatedDate = Intl.DateTimeFormat("ru-RU").format(date);
+
   return (
     <div className="journal-item">
       <h2 className="journal-item__title">{title}</h2>
       <h2 className="journal-item__body">
-        <div className="journal-item__date">{date.toString()}</div>
+        <div className="journal-item__date">{fromatedDate}</div>
         <div className="journal-item__text">{text}</div>
       </h2>
     </div>

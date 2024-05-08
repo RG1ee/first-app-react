4b93270a849ffcd7a8b640ee4ca7fbddc410c5f9
RG1ee <niwows@yandex.ru>
2024-05-08 18:09:29 +0300

    props

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,34 @@ import Button from "./components/Button/Button";
 import JournalItem from "./components/JournalItem/JournalItem";
 
 function App() {
+  const data = [
+    {
+      title: "Заголовок",
+      date: new Date(),
+      text: "Текст",
+    },
+    {
+      title: "Заголовок 2",
+      date: new Date(),
+      text: "Текст 2",
+    },
+  ];
+
   return (
     <>
       <h1>Заголовок</h1>
       <p>Текст</p>
       <Button />
-      <JournalItem />
+      <JournalItem
+        title={data[0].title}
+        date={data[0].date}
+        text={data[0].text}
+      />
+      <JournalItem
+        title={data[1].title}
+        date={data[1].date}
+        text={data[1].text}
+      />
     </>
   );
 }
--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,15 +1,11 @@
 import "./JournalItem.css";
 
-function JournalItem() {
-  const title = "Заголовок";
-  const date = "Дата";
-  const text = "Текст";
-
+function JournalItem({ title, date, text }) {
   return (
     <div className="journal-item">
       <h2 className="journal-item__title">{title}</h2>
       <h2 className="journal-item__body">
-        <div className="journal-item__date">{date}</div>
+        <div className="journal-item__date">{date.toString()}</div>
         <div className="journal-item__text">{text}</div>
       </h2>
     </div>
